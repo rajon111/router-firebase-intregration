7c27a99698cf1f5c9494facf4d5e00e5ec78f2a0
rajon <mostofarajon111@gmail.com>
2022-04-08 23:05:06 +0600

    react firebase intregration init

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyQQ_rS9WAa1eRizbX0R9KaaQaQMhIGPY",
+  authDomain: "router-firebase-intregat-6b8f8.firebaseapp.com",
+  projectId: "router-firebase-intregat-6b8f8",
+  storageBucket: "router-firebase-intregat-6b8f8.appspot.com",
+  messagingSenderId: "688627355664",
+  appId: "1:688627355664:web:714efb32e7eec34cddfa95"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOMClient.createRoot(document.getElementById('root'))
+
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
